dbf8bece530ef61ce439d3b29e41785763a66333
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-09-09 19:23:14 +0900

    接続うまくいかない

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show]
+  resources :users, only:[:show, :edit, :update]
 root :to => 'homes#top'   
 resources :posts do
   resources :comments, only:[:create, :destroy]
